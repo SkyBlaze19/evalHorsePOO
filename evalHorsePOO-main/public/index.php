d78c30aded45ab665d86c96cabc05b40ece1bc17
Theo Lemaigre <theolemaigrepro@hotmail.com>
2022-11-03 16:10:20 +0100

    Ajout code de test

--- evalHorsePOO-main/public/index.php
+++ evalHorsePOO-main/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once __DIR__ . "../src/app.php";
+
+$MonHumain = new Human("Théo", 21, "7", "chemin des quairières", "14763", "Bamervor");
+$Manager1 = new Manager("Claire", 36, "3", "Impasse du puits", "76500", "Honyeux", "Animaux");
+$EcurieBeaute = new Stable("Ecurie Beaute Fleury", "19", "Rue des ardennes", "14750", "Saint-contest");
+
+echo $MonHumain;
+echo "\n\nLe manager :\n" . $Manager1;
+echo "\n\nL'écurie :\n" . $EcurieBeaute;
