e160b0c7b1373e9066a72f733627b801a31358b5
Theo Lemaigre <theolemaigrepro@hotmail.com>
2022-11-03 14:39:08 +0100

    Création class Manager

--- evalHorsePOO-main/src/Controller/Manager.class.php
+++ evalHorsePOO-main/src/Controller/Manager.class.php
@@ -2,5 +2,62 @@
 
 class Manager
 {
+    // Propriétés 
+    private const ANONYME = "Inconnu";
 
+    private string $Name;
+    private int $Age;
+
+    // Constructeur
+
+    public function __construct(string $Name = self::ANONYME, int $Age = 30)
+    {
+        $this->setName($Name)
+            ->setAge($Age);
+    }
+
+    /**
+     * Get the value of Name
+     */ 
+    public function getName(): string
+    {
+        return $this->Name;
+    }
+
+    /**
+     * Set the value of Name
+     *
+     * @return  self
+     */ 
+    private function setName($Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Age
+     */ 
+    public function getAge(): int
+    {
+        return $this->Age;
+    }
+
+    /**
+     * Set the value of Age
+     *
+     * @return  self
+     */ 
+    private function setAge($Age): self
+    {
+        $this->Age = $Age;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return "Le manager s'appelle {$this->Name} et il à {$this->Age}\n";   
+    }
 }
