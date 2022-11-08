9ffdaced165b5073b2ca82e07380296fb375daeb
Theo Lemaigre <theolemaigrepro@hotmail.com>
2022-11-08 19:28:39 +0100

    Création class Animal et Equine (en partie (à completer avec les autres classes) )

--- evalHorsePOO-main/src/Controller/Class/Animal.php
+++ evalHorsePOO-main/src/Controller/Class/Animal.php
@@ -2,5 +2,33 @@
 
 abstract class Animal
 {
+    // Propriétés
+    protected string $name;
 
+    // Constructeur
+    public function __construct(string $name = "Inconnu")
+    {
+        $this->setName($name);
+    }
+
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    private function setName($name): self 
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
---
+++ evalHorsePOO-main/src/Controller/Class/Equine.php
@@ -0,0 +1,121 @@
+<?php
+
+abstract class Equine extends Animal
+{
+    // Propriétés
+
+    private string $id;
+    private string $color;
+    private int $water;
+
+    private array $equides = [];
+
+    //private Rider $Rider; //TODO
+    
+    // Constructeur
+    public function __construct(string $name, string $id)
+    {
+        parent::__construct($name);
+        $this->setId($this->idGenerator());
+    }
+
+    public function idGenerator()
+    {
+        return '000-' . substr($this->getName(), 0, 1) . "-" . substr($this->getColor(), 0, 1) . "-" . count($this->getEquines())."\n";
+    }
+
+    public function setCategory(string $var = null)
+    {
+        # code...
+    }
+
+    public function getCategory(int $category = "inconnue")
+    {
+        return $this->category;
+    }
+
+    public function setRider(string $var = null)
+    {
+        # code...
+    }
+
+    public function __toString(): string
+    {
+        $str = "Cet equidé est un : {$this->getCategory()}\n";
+        $str .= "C'est le numéro : {substr($this->id, -1)}\n";
+        $str .= "Il a besoin de {$this->water} L d'eau\n";
+        $str .= "Il a besoin de {$this->water} L d'eau\n";
+        /* Dès que Rider sera créer (A ajuster si besoin)
+        if($Rider != null)
+            $str.= "Il possède un rider qui est : {$this->getRider()}\n";
+        else
+            $str.= "Il ne possède pas de rider\n";
+        */
+        return $str;
+    }
+
+    public function getEquines()
+    {
+        return $this->equides;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    private function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of color
+     */ 
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set the value of color
+     *
+     * @return  self
+     */ 
+    private function setColor($color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of water
+     */ 
+    public function getWater(): int
+    {
+        return $this->water;
+    }
+
+    /**
+     * Set the value of water
+     *
+     * @return  self
+     */ 
+    private function setWater($water): self
+    {
+        $this->water = $water;
+
+        return $this;
+    }
+}
