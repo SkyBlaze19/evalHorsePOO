d9282bb5ce43bd56807e0163590e59a1a6e478e1
Theo Lemaigre <theolemaigrepro@hotmail.com>
2022-11-09 23:58:54 +0100

    Ajout classes RIder

--- evalHorsePOO-main/src/Controller/Class/Rider.php
+++ evalHorsePOO-main/src/Controller/Class/Rider.php
@@ -1,6 +1,54 @@
 <?php
 
-class Rider
-{
+use App\Controller\Class\Equine;
+use App\Controller\Class\Human;
+use Error;
 
+class Rider extends Human
+{
+    // Propriétés 
+    private Equine $Equine;
+
+    // Constructeurs
+    public function __construct
+    (
+        $name, 
+        $age,
+        $adress,
+        $street,
+        $postCode,
+        $city, 
+        Equine $equine)
+    {
+        parent::__construct
+        (
+        $name, 
+        $age,
+        $adress,
+        $street,
+        $postCode,
+        $city
+        );
+        $this->setEquine($equine);
+    }
+
+    /**
+     * Get the value of Equine
+     */ 
+    public function getEquine(): Equine
+    {
+        return $this->Equine;
+    }
+
+    /**
+     * Set the value of Equine
+     *
+     * @return  self
+     */ 
+    private function setEquine($Equine): self
+    {
+        $this->Equine = $Equine;
+
+        return $this;
+    }
 }
