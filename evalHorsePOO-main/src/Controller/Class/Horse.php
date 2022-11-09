a005c16c92940a56e385f3945dfce508779cdec7
Theo Lemaigre <theolemaigrepro@hotmail.com>
2022-11-09 13:57:11 +0100

    Création class Horse

--- evalHorsePOO-main/src/Controller/Class/Horse.php
+++ evalHorsePOO-main/src/Controller/Class/Horse.php
@@ -3,5 +3,69 @@ namespace App\Controller\Class;
 
 class Horse extends Equine
 {
-    
+    // Propriétés
+    private int $age;
+    // Propriétaire ? // A définir si utile ou non
+
+    // Constructeur
+    public function __construct(int $age, $name, $id, $water)
+    {
+        parent::__construct($name, $id, $water);
+        $this->setNom($age);
+    }
+
+    public function __toString(): string
+    {
+        $str = "Ce cheval s'appelle : {$this->name}\n";
+        $str .= "C'est le numéro : {substr($this->id, -1)}\n";
+        $str .= "Son identifiant complet est : $this->id}\n";
+        if ($this->getAge() == 1)
+            $str .= "Il a {$this->age} an\n";
+        elseif ($this->getAge() == 0)
+        $str .= "Il a moins d'un an\n";
+        else
+            $str .= "Il a {$this->age} ans\n";
+        $str .= "Il a besoin de {$this->water} litres d'eau.\n";
+        return $str;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    private function setNom($nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of age
+     */ 
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @return  self
+     */ 
+    private function setAge($age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
 }
